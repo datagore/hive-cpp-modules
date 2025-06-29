9f9dc4c0999691b766b7a537eaa5d9218ae41718
datagore <104622881+datagore@users.noreply.github.com>
2025-06-29 12:17:11 +0300

    Get started on cpp00/ex02

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,62 @@
+#include "Account.hpp"
+
+int Account::getNbAccounts(void)
+{
+	return 0;
+}
+
+int Account::getTotalAmount(void)
+{
+	return 0;
+}
+
+int Account::getNbDeposits(void)
+{
+	return 0;
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return 0;
+}
+
+void Account::displayAccountsInfos(void)
+{
+}
+
+Account::Account(int initialDeposit)
+{
+	(void) initialDeposit;
+}
+
+Account::~Account(void)
+{
+}
+
+void Account::makeDeposit(int deposit)
+{
+	(void) deposit;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	(void) withdrawal;
+	return false;
+}
+
+int Account::checkAmount(void) const
+{
+	return 0;
+}
+
+void Account::displayStatus(void) const
+{
+}
+
+void Account::_displayTimestamp(void)
+{
+}
+
+Account::Account(void)
+{
+}
