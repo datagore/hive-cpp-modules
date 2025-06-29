df66ddff75b0e498095248c13c443a0285d57778
datagore <104622881+datagore@users.noreply.github.com>
2025-06-29 22:22:09 +0300

    cpp01/ex05: Add exercise

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,22 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <string>
+
+class Harl
+{
+public:
+	Harl();
+	~Harl();
+	Harl(const Harl& other);
+	Harl& operator=(const Harl& other);
+	void complain(std::string level);
+
+private:
+	void debug(void);
+	void info(void);
+	void warning(void);
+	void error(void);
+};
+
+#endif // #ifndef HARL_HPP
