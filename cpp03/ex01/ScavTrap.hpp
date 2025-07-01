8efa05a8dc2f9b40b2aa4e13b8cf7eb5fd5604fd
datagore <104622881+datagore@users.noreply.github.com>
2025-07-02 00:03:38 +0300

    cpp03/ex01: Set up basic code for exercise

---
+++ cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,16 @@
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap: public ClapTrap
+{
+public:
+	ScavTrap();
+	~ScavTrap();
+	ScavTrap(const ScavTrap& other);
+	ScavTrap& operator=(const ScavTrap& other);
+	void guardGate();
+};
+
+#endif // #ifndef SCAVTRAP_HPP
