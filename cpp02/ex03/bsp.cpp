9a4d0a2901215facb9152d8401bab36e38b591c6
datagore <104622881+datagore@users.noreply.github.com>
2025-07-04 17:04:36 +0300

    cpp02/ex03: Fix BSP check

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -2,9 +2,10 @@
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	return Point::wedge(a, b, point) < 0
-	    && Point::wedge(b, c, point) < 0
-		&& Point::wedge(c, a, point) < 0;
+	bool ab = Point::wedge(a, b, point) < 0;
+	bool bc = Point::wedge(b, c, point) < 0;
+	bool ca = Point::wedge(c, a, point) < 0;
+	return ab == bc && bc == ca;
 }
 
 int main()
@@ -21,6 +22,22 @@ int main()
 	*/
 
 	if (bsp({-3, -3}, {0, 3}, {3, -3}, {0, 0}))
+		std::cout << "inside\n";
+	else
+		std::cout << "outside\n";
+
+	/*
+			   (0, 3)
+				  B
+				 / \
+				/   \     (Same triangle but with vertices reversed)
+			   /  P  \
+			  /       \
+	         A---------C
+       (-3, -3)       (3, -3)
+	*/
+
+	if (bsp({3, -3}, {0, 3}, {-3, -3}, {0, 0}))
 		std::cout << "inside\n";
 	else
 		std::cout << "outside\n";
