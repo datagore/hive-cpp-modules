97b30834d886be94072b3030108e31b15c01c2c2
datagore <104622881+datagore@users.noreply.github.com>
2025-08-26 22:58:51 +0300

    cpp08/ex01: Fix Span range issue found during eval

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -23,8 +23,8 @@ unsigned int Span::shortestSpan() const
     std::sort(numbers.begin(), numbers.begin() + storedNumbers);
     unsigned int shortest = UINT_MAX;
     for (size_t i = 1; i < storedNumbers; i++) {
-        long higher = static_cast<long>(numbers[i - 0]);
-        long lower  = static_cast<long>(numbers[i - 1]);
+        long higher = numbers[i - 0];
+        long lower  = numbers[i - 1];
         shortest = std::min<unsigned>(shortest, higher - lower);
     }
     return shortest;
@@ -34,7 +34,8 @@ unsigned int Span::longestSpan() const
 {
     if (storedNumbers < 2)
         throw std::logic_error("span is too short");
-    long max = *std::max_element(numbers.begin(), numbers.end());
-    long min = *std::min_element(numbers.begin(), numbers.end());
+    auto end = numbers.begin() + storedNumbers;
+    long max = *std::max_element(numbers.begin(), end);
+    long min = *std::min_element(numbers.begin(), end);
     return max - min;
 }
