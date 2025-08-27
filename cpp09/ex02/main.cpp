71286d99423bdd5dee8ed2ce29da78763b7fc772
datagore <104622881+datagore@users.noreply.github.com>
2025-08-27 17:59:18 +0300

    cpp09/ex02: Fix some range bugs

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,74 +6,6 @@
 
 #if 1
 
-// Move groups of `size` integers from the index `src` to the index `dst`,
-// shifting other elements to make room.
-
-template <class Container>
-void shift(Container& a, size_t dst, size_t src, size_t size)
-{
-    auto first = a.begin() + std::min(src, dst);
-    auto middle = a.begin() + src + (src < dst) * size;
-    auto last = a.begin() + std::max(src, dst) + size;
-    std::rotate(first, middle, last);
-}
-
-// Use binary search to insert the group of `size` integers at index `src` into
-// the start of the sequence, up to the index `max`.
-
-template <class Container>
-void insert(Container& n, size_t max, size_t src, size_t size)
-{
-    size_t min = 0;
-    while (min < max) {
-        size_t mid = (min + max) / 2;
-        if (n[mid * size + size - 1] < n[src * size + size - 1])
-            min = mid + 1;
-        else
-            max = mid - 0;
-    }
-    shift(n, min * size, src * size, size);
-}
-
-// Merge groups of `size` integers, first by moving odd-numbered ones to the
-// front of the sequence (including also the first group in the sequence), and
-// then repeatedly inserting the even-numbered ones into their right place using
-// binary search.
-
-template <class Container>
-void merge(Container& n, size_t size)
-{
-    size_t elems = n.size() / size;
-    size_t end = n.size() / (size * 2) + 1;
-    for (size_t i = 2; i < end; i++)
-        shift(n, i * size, i * size * 2 - size, size);
-    for (size_t src = end; src < elems; src++)
-        insert(n, end + size, src, size);
-}
-
-// Divide a sequence into groups of `size` numbers. Swap adjacent pairs of
-// groups so that the last number in the first group is always lower than the
-// last number in the second group.
-
-template <class Container>
-void divide(Container& n, size_t size)
-{
-    for (size_t i = 0; i < n.size() / size - 1; i += 2)
-        if (n[i * size + size - 1] > n[(i + 1) * size + size - 1])
-            shift(n, i * size, (i + 1) * size, size);
-}
-
-// Sort a sequence by recursively dividing and merging groups of numbers.
-
-template <class Container>
-void sort(Container& n, size_t size = 1)
-{
-    divide(n, size);
-    if (size * 4 < n.size())
-        sort(n, size * 2);
-    merge(n, size);
-}
-
 template <class Container>
 void print(const char* label, const Container& numbers)
 {
@@ -83,12 +15,92 @@ void print(const char* label, const Container& numbers)
     std::cout << "\n";
 }
 
-int main()
+// Get the nth Jacobstahl number.
+
+size_t jacobstahl(size_t n)
 {
-    std::vector numbers = {11, 2, 17, 0, 16, 8, 6, 15, 10, 3, 21, 1, 18, 9, 14, 19, 12, 5, 4, 20, 13, 7};
+    return ((1 << n) + ((n % 2) * 2 - 1)) / 3;
+}
+
+// Move groups of `count` integers from the index `src` to the index `dst`,
+// shifting other elements to make room.
+
+template <class Container>
+void shift(Container& a, size_t dst, size_t src, size_t count)
+{
+    auto first = a.begin() + std::min(src, dst);
+    auto middle = a.begin() + src + (src < dst) * count;
+    auto last = a.begin() + std::max(src, dst) + count;
+    std::rotate(first, middle, last);
+}
+
+// Use binary search to insert the group of `count` integers at index `src` into
+// the start of the sequence.
+
+template <class Container>
+void insert(Container& n, size_t src, size_t count)
+{
+    size_t min = 0;
+    size_t max = src;
+    while (min < max) {
+        size_t mid = (min + max) / 2;
+        if (n[mid * count + count - 1] < n[src * count + count - 1])
+            min = mid + 1;
+        else
+            max = mid - 0;
+    }
+    shift(n, min * count, src * count, count);
+}
+
+// Merge groups of `count` integers, first by moving odd-numbered ones to the
+// front of the sequence (including also the first group in the sequence), and
+// then repeatedly inserting the even-numbered ones into their right place using
+// binary search.
+
+template <class Container>
+void merge(Container& n, size_t count)
+{
+    size_t elems = n.size() / count;
+    size_t end = n.size() / (count * 2) + 1;
+    for (size_t i = 2; i < end; i++)
+        shift(n, i * count, i * count * 2 - count, count);
+    for (size_t src = end; src < elems; src++)
+        insert(n, src, count);
+}
+
+// Divide a sequence into groups of `count` numbers. Swap adjacent pairs of
+// groups so that the last number in the first group is always lower than the
+// last number in the second group.
+
+template <class Container>
+void divide(Container& n, size_t count)
+{
+    for (size_t i = 0; i < n.size() / count - 1; i += 2)
+        if (n[i * count + count - 1] > n[(i + 1) * count + count - 1])
+            shift(n, i * count, (i + 1) * count, count);
+}
+
+// Sort a sequence by recursively dividing and merging groups of numbers.
+
+template <class Container>
+void sort(Container& n, size_t count = 1)
+{
+    divide(n, count);
+    if (n.size() / (count * 2) > 0)
+        sort(n, count * 2);
+    merge(n, count);
+}
+
+int main(int argc, const char **argv)
+{
+    std::vector<int> numbers;
+    for (int i  = 1; i < argc; i++)
+        numbers.push_back(std::atoi(argv[i]));
     print("Before: ", numbers);
     sort(numbers);
     print("After:  ", numbers);
+    bool sorted = std::is_sorted(numbers.begin(), numbers.end());
+    printf("Sorted: %s\n" ANSI_RESET, sorted ? ANSI_GREEN "YES" : ANSI_RED "NO");
 }
 
 #else
